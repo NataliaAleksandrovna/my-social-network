6816e73ff394e8cd80b72c0e3bb1e3096537179c
NataliaAleksandrovna <111694299+NataliaAleksandrovna@users.noreply.github.com>
2023-03-07 08:56:12 +0300

    Updates

---
+++ precache-manifest.ce00d008599fa3905ba68e532ccfdd3f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c57506de59ee0b839d14",
+    "url": "/my-social-network/static/css/main.eeb2d5ed.chunk.css"
+  },
+  {
+    "revision": "c57506de59ee0b839d14",
+    "url": "/my-social-network/static/js/main.c57506de.chunk.js"
+  },
+  {
+    "revision": "0cd66f5ce4cea57e709f",
+    "url": "/my-social-network/static/js/1.0cd66f5c.chunk.js"
+  },
+  {
+    "revision": "625da990683f483ae8f1",
+    "url": "/my-social-network/static/css/2.8728bb01.chunk.css"
+  },
+  {
+    "revision": "625da990683f483ae8f1",
+    "url": "/my-social-network/static/js/2.625da990.chunk.js"
+  },
+  {
+    "revision": "ea2593aea53c090da331",
+    "url": "/my-social-network/static/js/3.ea2593ae.chunk.js"
+  },
+  {
+    "revision": "1cbdcc32f7bea9b9a56b",
+    "url": "/my-social-network/static/js/runtime~main.1cbdcc32.js"
+  },
+  {
+    "revision": "72747583e13c416370d5860752ebed1d",
+    "url": "/my-social-network/static/media/user.72747583.png"
+  },
+  {
+    "revision": "820f2dff710152d8918bcfe2b2596483",
+    "url": "/my-social-network/static/media/preloader.820f2dff.gif"
+  },
+  {
+    "revision": "a630ee3f40261dd82f1f88e03c4a8782",
+    "url": "/my-social-network/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/NataliaAleksandrovna.github.io/my-social-network/precache-manifest.66868468ea5b97bdf1155adaaa34643f.js"
+  "/my-social-network/precache-manifest.ce00d008599fa3905ba68e532ccfdd3f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/NataliaAleksandrovna.github.io/my-social-network/index.html", {
+workbox.routing.registerNavigationRoute("/my-social-network/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
